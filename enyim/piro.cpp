1be1e29c2324c8979f872b216c136c08f8a49e9b
topanka <33791367+topanka@users.noreply.github.com>
2019-07-14 23:59:53 +0200

    Add files via upload

--- enyim/piro.cpp
+++ enyim/piro.cpp
@@ -14,7 +14,8 @@ int piro_read(PIRO *ps)
   }
   if(ps->l_t0 == 0) {
     ps->l_t0=g_millis;
-    ps->l_t1=ps->l_t0+100;
+//    ps->l_t1=ps->l_t0+100;
+    ps->l_t1=ps->l_t0+20;
     ps->l_s=0;
     ps->l_n=0;
   }
@@ -26,10 +27,12 @@ int piro_read(PIRO *ps)
       ps->s_val0=ps->s_s/ps->s_n;
       ps->s_t0=0;
     
-      if((g_millis-ps->l_t1) >= 100) {
+//      if((g_millis-ps->l_t1) >= 100) {
+      if((g_millis-ps->l_t1) >= 20) {
         ps->l_s+=ps->s_val0;
         ps->l_n++;
-        if((g_millis-ps->l_t0) >= 1300) {
+//        if((g_millis-ps->l_t0) >= 1300) {
+        if((g_millis-ps->l_t0) >= 500) {
           ps->l_val0=ps->l_s/ps->l_n;
           ps->l_t0=0;
         }
